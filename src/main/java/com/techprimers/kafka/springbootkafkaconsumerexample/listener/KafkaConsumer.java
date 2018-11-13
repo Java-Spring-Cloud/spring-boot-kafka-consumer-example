c43ee6138e97a9f0334ea0ca6ba971e674dc3c2e
areddy13 <anjana.reddy13@t-mobile.com>
2018-11-12 17:46:59 -0800

    Added initial draft for reference purpose

---
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.techprimers.kafka.springbootkafkaconsumerexample.listener;
+
+import com.techprimers.kafka.springbootkafkaconsumerexample.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test", group = "testgroup")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "test_json", group = "testgroup1",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
